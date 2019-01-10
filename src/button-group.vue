d680c645296d45a9ffea4b891b9f3577c9340f43
xingkongs <ldk8259878@163.com>
2019-01-10 09:36:59 +0800

    添加 yarn start 使用 var变量 基本完成button

--- src/button-group.vue
+++ src/button-group.vue
@@ -12,22 +12,26 @@
 </script>
 
 <style lang="scss" scoped>
-.button-group{
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    vertical-align: middle;
-    border-radius: 0;
-    &:not(:first-of-type){
-        margin-left:-1px;
+    .button-group {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        vertical-align: middle;
+
+        > .gulu-button {
+            border-radius: 0;
+            &:not(:first-of-type) {
+                margin-left: -1px;
+            }
+            &:first-of-type {
+                border-top-left-radius: 4px;
+                border-bottom-left-radius: 4px;
+            }
+
+            &:last-of-type {
+                border-top-right-radius: 4px;
+                border-bottom-right-radius: 4px;
+            }
+        }
     }
-    &:first-of-type{
-        border-top-left-radius: 4px;
-        border-bottom-left-radius: 4px;
-    }
-    &:last-of-type{
-        border-top-right-radius: 4px;
-        border-bottom-right-radius: 4px;
-    }
-}
 </style>
--- src/gulu-button.vue
+++ src/gulu-button.vue
@@ -1,6 +1,5 @@
 <template>
-    <button class="gulu-button" :class="{[`icon-${iconPosition}`]:true}"
-    @click="$emit('click')">
+    <button class="gulu-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
         <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
         <g-icon class="icon loading" v-if="loading" name="loading"></g-icon>
         <div class="content">
@@ -11,47 +10,81 @@
 
 <script>
     import Icon from './gulu-icon'
+
     export default {
         name: "GuluButton",
-        props:{
-            icon:{},
-            loading:{
-                type:Boolean,
-                default:false
+        props: {
+            icon: {},
+            loading: {
+                type: Boolean,
+                default: false
             },
-            iconPosition:{
-                type:String,
-                default:'left',
-                validate(value){
-                    return value === 'left'|| value === 'right'
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validate(value) {
+                    return value === 'left' || value === 'right'
                 }
             }
         },
-        components:{
-            "g-icon":Icon
+        components: {
+            "g-icon": Icon
         }
     }
 </script>
 
 <style lang="scss" scoped>
     @keyframes loading {
-        0% {transform: rotate(0)}
-        100% {transform: rotate(360deg)}
+        0% {
+            transform: rotate(0)
+        }
+        100% {
+            transform: rotate(360deg)
+        }
     }
-    .gulu-button{
-        font-size: 14px;
+
+    .gulu-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
         display: inline-flex;
         align-items: center;
         justify-content: center;
         vertical-align: middle;
-        > .content{
-            order:2;
+
+        &:hover {
+            border-color: var(--border-color-hover);
         }
-        > .icon{
-            order:1;
-            margin-right:0.2em;
+
+        &:active {
+            background-color: var(--button-active-bg);
         }
-        .loading{
+
+        &:focus {
+            outline: none;
+        }
+
+        > .content {
+            order: 2;
+        }
+
+        > .icon {
+            order: 1;
+            margin-right: 0.2em;
+        }
+        &.icon-right{
+            >.content{
+                order:1;
+            }
+            >.icon{
+                order:2;
+                margin-left:0.2em;
+                margin-right:0;
+            }
+        }
+        .loading {
             animation: loading 1s infinite linear;
         }
     }
