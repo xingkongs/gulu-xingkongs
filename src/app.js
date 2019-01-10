ad40a0fa5f50c9f5f7e6ccf0c5abec002ed38a1d
xingkongs <ldk8259878@163.com>
2019-01-10 14:58:11 +0800

    使用chai expect 添加4个测试用例 第五个需要用到 mock

--- src/app.js
+++ src/app.js
@@ -17,3 +17,78 @@ new Vue({
     },
     methods: {}
 })
+
+import chai from 'chai'
+
+const expect  = chai.expect
+
+{
+    const constructor = Vue.extend(Button)
+    const button =new constructor({
+        propsData:{
+            icon:'settings'
+        }
+    }).$mount()
+    const useElement = button.$el.querySelector('use')
+    const href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq("#i-settings")
+    button.$destroy()
+}
+{
+    const constructor = Vue.extend(Button)
+    const button  = new constructor({
+        propsData:{
+            icon:'settings',
+            loading:true
+        }
+    }).$mount()
+    const useElement = button.$el.querySelector('use')
+    const href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    button.$destroy()
+}
+{
+    const div= document.createElement('div')
+    document.body.appendChild(div)
+    const constructor = Vue.extend(Button)
+    const button = new constructor({
+        propsData:{
+            icon:'settings'
+        }
+    }).$mount(div)
+    const svg = button.$el.querySelector('svg')
+    const {order} = window.getComputedStyle(svg)
+    expect(order).to.eq("1")
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const constructor = Vue.extend(Button)
+    const button = new constructor({
+        propsData:{
+            icon:'setting',
+            iconPosition:'right'
+        }
+    }).$mount(div)
+    const svg = button.$el.querySelector('svg')
+    const {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const construcutor = Vue.extend(Button)
+    const gButton = new construcutor({
+        propsData:{
+            icon:'settings'
+        }
+    }).$mount()
+    const button = gButton.$el
+    gButton.$on('click',function () {
+        console.log(1)
+    })
+    button.click()
+    gButton.$destroy()
+}
