f0909f1bf991f90081748651e8ba551fec81d41d
xingkongs <ldk8259878@163.com>
2019-01-11 10:18:39 +0800

    基本完成input

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Button from './gulu-button'
 import Icon from './gulu-icon'
 import ButtonGroup from './button-group'
+import ButtonInput from './gulu-input'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input',ButtonInput)
 
 new Vue({
     el: "#app",
@@ -13,7 +15,12 @@ new Vue({
         loading1: false,
         loading2: false,
         loading3: false,
+        message:"xxx"
 
     },
-    methods: {}
+    methods: {
+        inputChange(e){
+            console.log(e)
+        }
+    }
 })
---
+++ src/gulu-input.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="input--wrapper" :class="{error:error}">
+        <input type="text" :value="value" :disabled="disabled" :readonly="readonly"
+        @change="$emit('change',$event.target.value)"
+        @input="$emit('input',$event.target.value)"
+        @focus="$emit('focus',$event.target.value)"
+        @blur="$emit('blur',$event.target.value)">
+        <template v-if="error">
+            <g-icon class="icon-error" name="error"></g-icon>
+            <p class="error_message">{{error}}</p>
+        </template>
+    </div>
+</template>
+
+<script>
+    import Icon from './gulu-icon'
+
+    export default {
+        name: "GuluInput",
+        components: {
+            'g-icon': Icon
+        },
+        props: {
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            readonly: {
+                type: Boolean,
+                default: false
+            },
+            value: {
+                type: String
+            },
+            error: {
+                type: String
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "./style/var";
+
+    .input--wrapper {
+        font-size: $font-size;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+
+        > :not(:last-child) {
+            margin-right: 0.5em;
+        }
+
+        > input {
+            height: $height;
+            border: 1px solid $input-border-color;
+            padding: 8px 16px;
+            font-size: inherit;
+            &:hover {
+                border: 1px solid $input-border-color-hover;
+            }
+            &:focus {
+                background-color: #fefffe;
+                border-color: #3bb4f2;
+                outline: none;
+                box-shadow: $input-box-shadow;
+            }
+
+            &[disabled], &[readonly] {
+                pointer-events: none;
+                border-color: transparent;
+                cursor: not-allowed;
+                opacity: .45;
+                -webkit-box-shadow: none;
+                box-shadow: none;
+            }
+        }
+
+        &.error {
+            color: #dd514c;
+
+            > .icon-error {
+                fill: #dd514c;
+            }
+
+            > input {
+                &:focus {
+                    border-color: #dd514c;
+                    box-shadow: $input-box-shadow-error;
+                    outline: none;
+                }
+            }
+        }
+    }
+</style>
