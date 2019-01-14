7f9bed835653a119b513f798909bd0c03a22bbcc
xingkongs <ldk8259878@163.com>
2019-01-14 17:11:41 +0800

    添加关闭动画

--- src/layout/sider.vue
+++ src/layout/sider.vue
@@ -1,15 +1,35 @@
 <template>
-    <div class="g-sider">
-        <slot></slot>
-    </div>
+    <transition name="slide">
+        <div class="g-sider" v-if="visibie">
+            <slot></slot>
+            <button @click="visibie = false">X</button>
+        </div>
+    </transition>
+
 </template>
 <script>
     export default {
-        name: "GuluSider"
+        name: "GuluSider",
+        data(){
+            return {
+                visibie:true
+            }
+        }
     }
 </script>
 <style scoped lang="scss">
     .g-sider{
-
+        position: relative;
+    >button{
+        position: absolute;
+        top:0;
+        right:0;
+    }
+    }
+    .slide-enter-active,.slide-leave-active{
+        transition:margin 0.5s ease-in;
+    }
+    .slide-enter,.slide-leave-to{
+        margin-left:-300px;
     }
 </style>
