f3d8111982edef3ea1e49ae76771f24f0322fce7
xingkong <ldk82598789@163.com>
2019-01-18 09:19:53 +0800

    创建popover

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ import GuluTabsHead from './tabs/gulu-tabs-head'
 import GuluTabsBody from './tabs/gulu-tabs-body'
 import GuluTabsItem from './tabs/gulu-tabs-item'
 import GuluTabsPane from './tabs/gulu-tabs-pane'
+import GuluPopover from './gulu-popover'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -36,6 +37,7 @@ Vue.component('g-tabs-head',GuluTabsHead)
 Vue.component('g-tabs-body',GuluTabsBody)
 Vue.component('g-tabs-item',GuluTabsItem)
 Vue.component('g-tabs-pane',GuluTabsPane)
+Vue.component('g-popover',GuluPopover)
 
 new Vue({
     el: "#app",
---
+++ src/gulu-popover.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="g-popover" @click="clickPopover">
+        <div class="content--wrapper" v-if="visible">
+            <slot name="content"></slot>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "gulu-popover",
+        data(){
+            return {
+                visible:false
+            }
+        },
+        methods:{
+            clickPopover(){
+                this.visible = !this.visible
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+.g-popover{
+    display: inline-block;
+    vertical-align: top;
+    position: relative;
+    & .content--wrapper{
+        position: absolute;
+        bottom:100%;
+        left:0;
+        border:1px solid red;
+    }
+}
+</style>
