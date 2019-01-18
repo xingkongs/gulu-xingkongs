93b5ac9937138b9204bc2bfdd74423671d2f5073
xingkong <ldk82598789@163.com>
2019-01-18 17:58:23 +0800

    利用表驱动编程重构代码

--- src/gulu-popover.vue
+++ src/gulu-popover.vue
@@ -35,26 +35,27 @@
                 const {contentWrapper, triggerWrapper} = this.$refs
                 document.body.appendChild(contentWrapper)
                 let {width, height, left, top} = triggerWrapper.getBoundingClientRect()
-                if (this.position === 'top') {
-                    contentWrapper.style.left = left + window.scrollX + 'px'
-                    contentWrapper.style.top = top + window.scrollY + 'px'
+                let {height: height2} = contentWrapper.getBoundingClientRect()
+                let positions = {
+                    top: {
+                        left: left + window.scrollX,
+                        top: top + window.scrollY
+                    },
+                    bottom: {
+                        left: left + window.scrollX,
+                        top: top + height + window.scrollY
+                    },
+                    left: {
+                        left: left + window.scrollX,
+                        top: top + window.scrollY + (height - height2) / 2
+                    },
+                    right: {
+                        left: left + width + window.scrollX,
+                        top: top + window.scrollY + (height - height2) / 2
+                    },
                 }
-                if (this.position === 'bottom') {
-                    contentWrapper.style.left = left + window.scrollX + 'px'
-                    contentWrapper.style.top = top + height + window.scrollY + 'px'
-                }
-                if (this.position === 'left') {
-                    contentWrapper.style.left = left + window.scrollX + 'px'
-                    let {height: height2} = contentWrapper.getBoundingClientRect()
-                    contentWrapper.style.top = top + window.scrollY + (height - height2) / 2 + 'px'
-                }
-                if (this.position === 'right') {
-                    contentWrapper.style.left = left + width + window.scrollX + 'px'
-                    let {height: height2} = contentWrapper.getBoundingClientRect()
-                    contentWrapper.style.top = top + window.scrollY + (height - height2) / 2 + 'px'
-                }
-
-
+                contentWrapper.style.left = positions[this.position].left + 'px'
+                contentWrapper.style.top = positions[this.position].top + 'px'
             },
             onClickDocument(e) {
                 if (!(this.$refs.contentWrapper && this.$refs.contentWrapper.contains(e.target))) {
@@ -174,6 +175,5 @@
                 border-right-color: #fff;
             }
         }
-
     }
 </style>
