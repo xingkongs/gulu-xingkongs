28338e0ea79b76b810db0dcea41bfcae8c7900fe
xingkong <ldk82598789@163.com>
2019-01-18 10:34:50 +0800

    将popover 弹层移动到body

--- src/gulu-popover.vue
+++ src/gulu-popover.vue
@@ -1,50 +1,65 @@
 <template>
     <div class="g-popover" @click.stop="clickPopover">
-        <div class="content--wrapper" v-if="visible" @click.stop>
+        <div ref="contentWrapper" class="content--wrapper" v-if="visible" @click="clickContent">
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 
 <script>
     export default {
         name: "gulu-popover",
-        data(){
+        data() {
             return {
-                visible:false
+                visible: false
             }
         },
-        methods:{
-            clickPopover(){
+        mounted() {
+
+        },
+        methods: {
+            clickPopover() {
                 this.visible = !this.visible
-                if(this.visible === true){
-                    this.$nextTick(()=>{
-                        let eventHandle = () =>{
-                            if(this.visible === true){
+                if (this.visible === true) {
+                    this.$nextTick(() => {
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        let {width, height, left, top} = this.$refs.triggerWrapper.getBoundingClientRect()
+                        this.$refs.contentWrapper.style.left = left + 'px'
+                        this.$refs.contentWrapper.style.top = top + 'px'
+                        let eventHandle = () => {
+                            if (this.visible === true) {
                                 this.visible = false
-                                document.removeEventListener('click',eventHandle)
+                                document.removeEventListener('click', eventHandle)
                             }
                         }
-                        document.addEventListener('click',eventHandle)
+                        document.addEventListener('click', eventHandle)
                     })
-                }else{
+                } else {
                 }
+            },
+            clickContent() {
+                console.log('yyy');
             }
         }
     }
 </script>
 
 <style scoped lang="scss">
-.g-popover{
-    display: inline-block;
-    vertical-align: top;
-    position: relative;
-    & .content--wrapper{
-        position: absolute;
-        bottom:100%;
-        left:0;
-        border:1px solid red;
+    .g-popover {
+        display: inline-block;
+        vertical-align: top;
+        position: relative;
+
     }
-}
+
+    .content--wrapper {
+        position: fixed;
+        top: 0;
+        left: 0;
+        border: 1px solid red;
+        transform:translateY(-100%);
+    }
 </style>
