2d74b791a31f18f01e714293cd9ba05d31c5ac6c
xingkong <ldk82598789@163.com>
2019-01-20 22:19:15 +0800

    添加popover position 测试

--- test/popover.test.js
+++ test/popover.test.js
@@ -1,6 +1,7 @@
 const expect = chai.expect
 import Vue from 'vue'
 import Popover from '../src/gulu-popover'
+Vue.component('g-popover',Popover)
 
 
 Vue.config.devtools = false
@@ -10,8 +11,8 @@ describe('Popover', () => {
     it('Popover 存在', () => {
         expect(Popover).to.exist
     })
-    describe('props', (done) => {
-        xit('接受 position 属性', () => {
+    describe('props', () => {
+        it('接受 position 属性', (done) => {
             const div = document.createElement('div')
             document.body.appendChild(div)
             div.innerHTML = `
@@ -20,14 +21,17 @@ describe('Popover', () => {
                 <button>点我</button>
             </g-popover>
             `
+            let vm = new Vue({
+                el: div
+            })
+            vm.$el.querySelector('button').click()
+            vm.$nextTick(() => {
+                let {contentWrapper} = vm.$refs.pop.$refs
+                expect(contentWrapper.classList.contains('position-bottom')).to.eq(true)
+                done()
+            })
         })
-        let vm = new Vue({
-            el: div
-        })
-        vm.$nextTick(() => {
-            console.log(vm.$refs);
-            done()
-        })
+
         it('接受 trigger 属性', () => {
 
         })
