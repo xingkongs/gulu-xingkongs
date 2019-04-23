d8bf1a1b17f9dbe9f0fb78e041fde0a4b3001cf2
xingkongs <ldk8259878@163.com>
2019-04-23 11:41:08 +0800

    更新 popover 测试

---
+++ tests/unit/popover.spec.js
@@ -0,0 +1,58 @@
+import chai, {expect} from 'chai';
+import {shallowMount, mount} from '@vue/test-utils';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import Vue from 'vue';
+chai.use(sinonChai);
+import Popover from '../../src/gulu-popover'
+Vue.component('g-popover',Popover)
+
+
+Vue.config.devtools = false
+Vue.config.productionTip = false
+
+describe('Popover', () => {
+    it('Popover 存在', () => {
+        expect(Popover).to.exist
+    })
+    describe('props', () => {
+        it('接受 position 属性', (done) => {
+            const wrapper = mount(Popover,{
+                propsData: {
+                    position: 'bottom'
+                },
+                slots: {
+                    default: '<button>点我</button>',
+                    content: "popover",
+                }
+            })
+            const button = wrapper.find('button')
+            button.trigger('click')
+            wrapper.vm.$nextTick(() => {
+                let contentWrapper = wrapper.find({ref:'contentWrapper'})
+                expect(contentWrapper.element.classList.contains('position-bottom')).to.eq(true)
+                button.trigger('click')//销毁 contentWrapper
+                done()
+            })
+        })
+
+        it('接受 trigger 属性', (done) => {
+            const wrapper = mount(Popover,{
+                propsData: {
+                    trigger:'hover'
+                },
+                slots: {
+                    default: '<button>点我</button>',
+                    content: "popover",
+                }
+            })
+            wrapper.find({ref:'popover'}).trigger('mouseenter')
+            wrapper.vm.$nextTick(() => {
+                let contentWrapper =document.body.querySelector('.content--wrapper')
+                //console.log(contentWrapper.outerHTML);
+                expect(contentWrapper.classList.contains('position-top')).to.eq(true)
+                done()
+            })
+        })
+    })
+})
