e1ba4a2bf803b79bf5cd3a0132ce2a480c228e45
xingkongs <ldk8259878@l63.com>
2019-05-10 16:56:48 +0800

    var.scss更新排序

---
+++ src/cascader/gulu-cascader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="cascader">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GuluCascader"
+    };
+</script>
+
+<style scoped lang="scss">
+    @import "../style/var";
+    .cascader{
+
+    }
+</style>
