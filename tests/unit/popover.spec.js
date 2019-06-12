68bb7fb5abffe7437cc228c41c69263922f2cf89
xingkongs <ldk8259878@l63.com>
2019-06-12 09:57:32 +0800

    更新popover trigger test

--- tests/unit/popover.spec.js
+++ tests/unit/popover.spec.js
@@ -1,58 +1,55 @@
-import chai, {expect} from 'chai';
-import {shallowMount, mount} from '@vue/test-utils';
-import sinon from 'sinon';
-import sinonChai from 'sinon-chai';
-import Vue from 'vue';
+import chai, {expect} from "chai";
+import {shallowMount, mount} from "@vue/test-utils";
+import sinonChai from "sinon-chai";
+import Vue from "vue";
 chai.use(sinonChai);
-import Popover from '../../src/gulu-popover'
-Vue.component('g-popover',Popover)
-
-
-Vue.config.devtools = false
-Vue.config.productionTip = false
-
-describe('Popover', () => {
-    it('Popover 存在', () => {
-        expect(Popover).to.exist
-    })
-    describe('props', () => {
-        it('接受 position 属性', (done) => {
-            const wrapper = mount(Popover,{
+import Popover from "../../src/gulu-popover";
+Vue.component("g-popover", Popover);
+Vue.config.devtools = false;
+Vue.config.productionTip = false;
+describe("Popover", () => {
+    it("Popover 存在", () => {
+        expect(Popover).to.exist;
+    });
+    describe("props", () => {
+        it("接受 position 属性", (done) => {
+            const wrapper = mount(Popover, {
                 propsData: {
-                    position: 'bottom'
+                    position: "bottom"
                 },
                 slots: {
-                    default: '<button>点我</button>',
+                    default: "<button>点我</button>",
                     content: "popover",
                 }
-            })
-            const button = wrapper.find('button')
-            button.trigger('click')
+            });
+            const button = wrapper.find("button");
+            button.trigger("click");
             wrapper.vm.$nextTick(() => {
-                let contentWrapper = wrapper.find({ref:'contentWrapper'})
-                expect(contentWrapper.element.classList.contains('position-bottom')).to.eq(true)
-                button.trigger('click')//销毁 contentWrapper
-                done()
-            })
-        })
-
-        it('接受 trigger 属性', (done) => {
-            const wrapper = mount(Popover,{
+                let contentWrapper = wrapper.find({ref: "contentWrapper"});
+                expect(contentWrapper.element.classList.contains("position-bottom")).to.eq(true);
+                button.trigger("click");//销毁 contentWrapper
+                done();
+            });
+        });
+        it("接受 trigger 属性", (done) => {
+            const wrapper = mount(Popover, {
                 propsData: {
-                    trigger:'hover'
+                    trigger: "hover"
                 },
                 slots: {
-                    default: '<button>点我</button>',
+                    default: "<button>点我</button>",
                     content: "popover",
                 }
-            })
-            wrapper.find({ref:'popover'}).trigger('mouseenter')
+            });
+            let contentWrapper = wrapper.find(".content--wrapper").element;
+            expect(contentWrapper).to.not.exist;
+            wrapper.find({ref: "popover"}).trigger("mouseenter");
             wrapper.vm.$nextTick(() => {
-                let contentWrapper =document.body.querySelector('.content--wrapper')
+                let contentWrapper = document.body.querySelector(".content--wrapper");
                 //console.log(contentWrapper.outerHTML);
-                expect(contentWrapper.classList.contains('position-top')).to.eq(true)
-                done()
-            })
-        })
-    })
-})
+                expect(contentWrapper.classList.contains("position-top")).to.eq(true);
+                done();
+            });
+        });
+    });
+});
